456c85e3db2bf2e6d883e8f1abe46d7b57981ac0
Gabohosorio <gabo.a.o.h@gmail.com>
2026-09-15 16:16:29 -0500

    Update 20260915

---
+++ myframework/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?
+  class Home{
+    
+    public function __construct()
+    {
+      //throw new \Exception('Not implemented');
+    }
+
+    public function home($params){
+      echo "Bienvendio a la página de inicio";
+    }
+  }//End class Home
+?>
